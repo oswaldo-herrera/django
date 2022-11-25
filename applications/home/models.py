f291846e60514126a58e0b6169a4ab94ab31f390
Oswaldo Herrera <oswaldo.herrera.mx@gmail.com>
2022-11-24 23:00:33 -0600

    noche

--- applications/home/models.py
+++ applications/home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Prueba(models.Model):
+    titulo = models.CharField(max_length=30)
+    subtitulo = models.CharField(max_length=50)
